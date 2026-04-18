fcc078563a6ec7d13e0ad68683d15060752804d1
Danila Poyarkov <dev@dannote.net>
2026-04-18 14:55:50 +0300

    Add right padding to code node line numbers
      │ 1 │ code     (was │ 1│ code)
      │ 98 │ code    (was │ 98│ code)

--- lib/boxart/code_node.ex
+++ lib/boxart/code_node.ex
@@ -62,8 +62,8 @@ defmodule Boxart.CodeNode do
       |> Enum.map(&Boxart.Utils.display_width/1)
       |> Enum.max(fn -> 0 end)
 
-    # left_pad + gutter + separator(│) + space + code
-    width = gutter_width + 4 + max_code_width
+    # left_pad + gutter + right_pad + separator(│) + space + code
+    width = gutter_width + 5 + max_code_width
 
     %CodeLabel{
       lines: lines,
@@ -90,7 +90,7 @@ defmodule Boxart.CodeNode do
 
     # Draw the gutter separator as a continuous vertical line
     {gutter_w, _} = gutter_width(code_label)
-    sep_col = content_start_x + gutter_w + 1
+    sep_col = content_start_x + gutter_w + 2
 
     canvas = draw_gutter_separator(canvas, sep_col, y, height)
 
