a34b573e9d1ddf87f4dd41cacfe824552495f6a0
Danila Poyarkov <dev@dannote.net>
2026-04-18 09:56:15 +0300

    Initial commit

---
+++ lib/boxart.ex
@@ -0,0 +1,18 @@
+defmodule Boxart do
+  @moduledoc """
+  Documentation for `Boxart`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Boxart.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/boxart_test.exs
@@ -0,0 +1,8 @@
+defmodule BoxartTest do
+  use ExUnit.Case
+  doctest Boxart
+
+  test "greets the world" do
+    assert Boxart.hello() == :world
+  end
+end
